3b6b15992674ad83758b3cab2ac4954e5c0e451f
nuvivicky <jothiram@ch1104s-mac-mini.local>
2017-04-03 16:13:47 +0530

    Project created and source code uploaded to GitHub

---
+++ Sample-Jenkins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample-Jenkins
+//
+//  Created by CH1104 on 03/04/17.
+//  Copyright © 2017 CH1104. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
